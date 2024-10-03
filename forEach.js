8e47580eaaf816bd365a2875ba8d4cdeb9c281a0
hassan <hassanibrahimndahiro@gmail.com>
2024-10-03 16:47:06 +0300

    function forEach

---
+++ forEach.js
@@ -0,0 +1,61 @@
+// forEach() =  // It takes two arguments: the array to iterate over and a callback function
+//             // The callback function takes three arguments: the current element, its index, and the array itself
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// let numbers =[1, 2, 3, 4, 5,];
+
+// numbers.forEach(triple);
+// // numbers.forEach(display);
+
+//   function double(element){
+//     console.log(element * 2);  // multiply each element by 2 and log it
+//   }  
+       
+// // function display(elemnt){
+// //     console.log(elemnt);
+// // }
+
+// //function to triple
+
+// function triple(element){
+//     console.log(element * 3);
+    
+// }
+
+// making our elements to fruits
+
+const fruits = ['apple', 'banana', 'orange', 'grape'];
+
+// fruits.forEach(display);
+
+// function display(element){
+//     console.log("This is a ", element);
+// }
+
+//using .toUppercase to capitalize the first letters of our array
+
+// fruits.forEach(capitalize);
+
+// function capitalize(element){
+//     console.log(element);
+    
+// }
+// fruits.forEach(upperCase);
+// function upperCase(element){
+//     // array[index] = element.toUpperCase(element);
+//     console.log(element.toUpperCase(element));
+    
+// }
+// fruits.forEach(display);
+// function display(element){
+//     console.log(element);
+    
+// }
+
+// fruits.forEach(capitalize);
+
+// function capitalize(element){
+//     console.log(element.charAt(0,).toUpperCase() + element.slice(1));
+    
+// }
